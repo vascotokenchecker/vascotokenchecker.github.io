68b72ad2729392977227df3e8294a4fecb9a5efb
vascotokenchecker <115654893+vascotokenchecker@users.noreply.github.com>
2022-10-12 14:02:02 -0300

    Add files via upload

---
+++ titleanime.js
@@ -0,0 +1,15 @@
+let x = 0;
+const title = []
+const nick = 'Droou Token Checker'
+for (let i = 1; i <= nick.length; ++i) {
+title.push(nick.slice(0, i))
+}
+for (let i = 1; i < nick.length; ++i) {
+title.push(nick.slice(0, -i))
+}
+title.push('&lrm;')
+function loop() {
+    document.getElementsByTagName('title')[0].innerHTML = title[x++ % title.length];
+    setTimeout(() => loop(), 0)
+}
+loop()
